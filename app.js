60ab0f80c3dfb43256075172ebdaaca2bb005173
t-kiryu <k040505t@gmail.com>
2022-11-15 13:57:34 +0000

    javascript基礎

---
+++ app.js
@@ -0,0 +1,63 @@
+// 変数の書き方
+    // let hello = 'Hello World';
+    // alert(hello);
+
+
+// 変数に数値、文字列を代入
+    // let int1 = 1;
+    // let float1 = 3.14;
+    // let str1 = 'JavaScriptを覚えよう';
+
+
+// 四則演算
+    // alert(4 + 3);
+    // alert(8 - 5);
+    // alert(2 * 6);
+    // alert(10 / 2);
+
+
+// 文字列の結合
+    // let str1 = 'Hello';
+    // let str2 = 'World!!';
+    // alert(str1 + str2);
+
+
+// 条件分岐(if文)
+    // let orange = 100;
+    // let apple = 120;
+
+    // if(orange < apple){
+    //   alert('みかんの値段がりんごより安い');
+    // }else if(orange == apple){
+    //   alert('みかんとりんごが同じ値段');
+    // }else{
+    //   alert('みかんの値段がりんごより高い');
+    // }
+
+
+// 繰り返し処理(while文, for文)
+  // while文 => trueの場合、無制限に繰返し
+      // let max = 100;
+      // let num = 1;
+      // let count = 0;
+
+      // while(num < max){
+      //   num = num * 2;
+      //   count = count + 1;
+      // }
+
+      // alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + 'です')
+
+
+  // do...while文 => 最初の1回目は処理、2回目以降は条件式を評価
+
+
+  // for文 => 決められた回数の処理繰返し(初期値、条件達成の記載必須)
+      // let i;
+      // let num = 0;
+
+      // for(i = 1; i < 11; i++){
+      //   num = num + i;
+      // }
+
+      // alert('1から10まで足し算した結果は' + num + 'です');
