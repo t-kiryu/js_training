85be8e1612ab368c0a1a590d27eaad7ea72b557b
t-kiryu <k040505t@gmail.com>
2022-11-20 14:58:14 +0000

    [add]要素スタイル変更

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -1,10 +1,25 @@
-// HTML読込後にjQueryが実行されるよう記述
-    // $(document).ready(function () {
-    //   $('body').html('<h1>Hello jQuery!!</h1>');
-    // });
+// jQueryの基本記述
+    // HTML読込後にjQueryが実行されるよう記述
+        // $(document).ready(function () {
+        //   $('body').html('<h1>Hello jQuery!!</h1>');
+        // });
+
+    // 省略形
+        // $(function () {
+        //   $('body').html('<h1>Hello jQuery!!</h1>');
+        // });
 
 
-// 上記の省略形
-    // $(function () {
-    //   $('body').html('<h1>Hello jQuery!!</h1>');
-    // });
+// div要素のスタイル変更
+        // $(function () {
+        //   $('.box1').css({
+        //     'background-color': '#0000FF',
+        //     'height': '100px'
+        //   })
+        // })
+
+    // 1つのcss記述の場合
+        // $('.box1').css('background-color', '#0000FF');
+
+
+// アニメーション追加
