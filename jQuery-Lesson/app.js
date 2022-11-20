1f8a45c9242a3f2a162b1abdf06de340856beb01
t-kiryu <k040505t@gmail.com>
2022-11-20 14:05:19 +0000

    jQuery学習開始

---
+++ jQuery-Lesson/app.js
@@ -0,0 +1,10 @@
+// HTML読込後にjQueryが実行されるよう記述
+    // $(document).ready(function () {
+    //   $('body').html('<h1>Hello jQuery!!</h1>');
+    // });
+
+
+// 上記の省略形
+// $(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
