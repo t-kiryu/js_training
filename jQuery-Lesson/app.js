de073f111a7c8adbf9b85997161919bc7572f848
t-kiryu <k040505t@gmail.com>
2022-11-21 04:01:38 +0000

    アニメーション学習

--- jQuery-Lesson/app.js
+++ jQuery-Lesson/app.js
@@ -11,6 +11,7 @@
 
 
 // div要素のスタイル変更
+    // 複数のcss記述の場合
         // $(function () {
         //   $('.box1').css({
         //     'background-color': '#0000FF',
@@ -22,4 +23,36 @@
         // $('.box1').css('background-color', '#0000FF');
 
 
-// アニメーション追加
+// アニメーション(上から下へスライド)
+        // $(function () {
+        //   $('.box1').slideDown();
+        // })
+
+
+// アニメーション(下から上へスライド)
+        // $(function () {
+        //     $('.box1').slideUp();
+        // })
+
+
+// アニメーション(要素の表示)
+        // $(function () {
+        //   $('.box1').show();
+        //   $('.box1').css({'background-color': '#0000FF'});
+        // })
+
+
+// アニメーション(要素の非表示)
+        // $(function () {
+        //     $('.box1').hide();
+        // })
+
+// 確認問題
+$(function () {
+  $('.box1').slideDown();
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'height': '100px',
+    'width': '200px'
+  })
+})
