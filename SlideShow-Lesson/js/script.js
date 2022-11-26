f79b6cca799c9c2056a9529bb5713d9a6c007ffa
t-kiryu <k040505t@gmail.com>
2022-11-26 03:14:58 +0000

    コメント編集

--- SlideShow-Lesson/js/script.js
+++ SlideShow-Lesson/js/script.js
@@ -16,18 +16,18 @@ $(".theTarget").skippr({
   // 子要素の種類("div" or "img")
   childrenElementType : 'div',
 
-  // ナビゲーション矢印の表示(trueで表示)
+  // ナビゲーション矢印表示(trueで表示)
   arrows : true,
 
-  // スライドショーの自動再生(falseで自動再生なし)
+  // スライドショー自動再生(falseで自動再生なし)
   autoPlay : true,
 
   // 自動再生時のスライド切替間隔(ミリ秒)
   autoPlayDuration : 5000,
 
-  // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+  // キーボード矢印キーでスライド送り設定(trueで有効)
   keyboardOnAlways : true,
 
-  // 1枚目のスライド表示時に戻る矢印を表示するかどうか [false]:矢印を隠さない [true]:矢印を隠す
+  // 1枚目スライド表示時に戻る矢印を表示([false]:矢印表示, [true]:矢印隠す)
   hidePrevious : false
 });
