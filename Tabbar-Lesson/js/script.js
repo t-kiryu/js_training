1a6731e050edb28f50ce8d8cdadd5361af8b20dd
t-kiryu <k040505t@gmail.com>
2022-11-28 03:59:40 +0000

    学習内容更新・タブメニュー

--- Tabbar-Lesson/js/script.js
+++ Tabbar-Lesson/js/script.js
@@ -1,21 +1,38 @@
-// アイコン押下時のメニュー表示
-$(function() {
-  $('.menu-trigger').on('click', function(event) {
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
-  });
+// タブ押下時のテキスト等表示
+
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $('#tab-contents .tab').hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
 })
 
 
+
 // 3行目
-    // menu-triggerクリック時にメニューが開くイベント発生
+    // 特定の属性を持つ要素や属性値の絞り込み可能 => $('要素[属性]')
+    // 属性値が特定の値に等しい要素だけを取得 => $('要素[属性 = "値"]')
+    // 属性値が特定の値に等しくない要素だけを取得 => $('要素[属性 != "値"]')
+    // .hide() => 特定のhtmlを非表示する
 
-// 4行目
-    // (this) => menu-trigger内にあるクリックされた要素(メニューアイコン)
-    // .toggleClass() => class属性切替
-    // 該当class属性あり:class削除, 該当class属性なし:class追加
-    // html内にはないがcss内にあり、ボタン押下時に切り替わる仕組み
 
-// 5行目
-    // .fadeToggle() => メニュー開閉時にフェードイン、フェードアウト
+// 6行目
+    // タブをクリックしたら非表示
+
+// 7行目
+    // id="tab-contents"内のいずれかのタブがクリックされた時、そのタブ内コンテンツを非表示
+    // .remeveClass() => html要素からclassを削除(activeを削除対象にしている)
+    // style.css内に#tab-menu .activeを記載しており、activeの時は色が変わるようにしていて、タブ切替時にclassを削除したい
+    //
+
+// 8行目
+    // .addClass() => html要素にclass属性を追加可能
+    // ("active")を追加対象にしており、$(this)=タブクリック時にactiveを追加する
+
+// 9行目
+    // .attr() => html要素の属性を取得可能(href属性を取得している)
+    // .show() => 要素表示が可能で、取得したhref属性の中身を表示
+    // $(this)の時 = タブクリック時に表示
